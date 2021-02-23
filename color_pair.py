d5e9f7038aafdff94cfde01f58f33c264a6cf0ad
Vivek Tak <57911834+wanderer-soul92@users.noreply.github.com>
2021-02-24 01:27:25 +0530

    Update color_pair.py

--- color_pair.py
+++ color_pair.py
@@ -1,22 +1,25 @@
-import color_codes as g
+
+from color_codes import MAJOR_COLORS
+from color_codes import MINOR_COLORS
 
 def get_color_from_pair_number(pair_number):
   zero_based_pair_number = pair_number - 1
-  major_index = zero_based_pair_number // g.get_length_major_colors()
-  if major_index >= g.get_length_major_colors():
+  major_index = zero_based_pair_number // len(MINOR_COLORS)
+  if major_index >= len(MAJOR_COLORS):
     raise Exception('Major index out of range')
-  minor_index = zero_based_pair_number % g.get_length_minor_colors()
-  if minor_index >= g.get_length_minor_colors():
+  minor_index = zero_based_pair_number % len(MINOR_COLORS)
+  if minor_index >= len(MINOR_COLORS):
     raise Exception('Minor index out of range')
-  return g.get_major_colors_index(major_index), g.get_minor_colors_index(minor_index)
+  return MAJOR_COLORS[major_index], MINOR_COLORS[minor_index]
+
 
 def get_pair_number_from_color(major_color, minor_color):
   try:
-    major_index = g.get_major_colors_value(major_color)
+    major_index = MAJOR_COLORS.index(major_color)
   except ValueError:
     raise Exception('Major index out of range')
   try:
-    minor_index = g.get_minor_colors_value(minor_color)
+    minor_index = MINOR_COLORS.index(minor_color)
   except ValueError:
     raise Exception('Minor index out of range')
-  return major_index * g.get_length_minor_colors() + minor_index + 1
+  return major_index * len(MINOR_COLORS) + minor_index + 1
