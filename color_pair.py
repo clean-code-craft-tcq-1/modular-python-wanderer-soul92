a8f4a06ec0d997d246f03518db024427d825d5b0
Vivek Tak <57911834+wanderer-soul92@users.noreply.github.com>
2021-02-24 00:53:04 +0530

    Add files via upload

---
+++ color_pair.py
@@ -0,0 +1,22 @@
+import color_codes as g
+
+def get_color_from_pair_number(pair_number):
+  zero_based_pair_number = pair_number - 1
+  major_index = zero_based_pair_number // g.get_length_major_colors()
+  if major_index >= g.get_length_major_colors():
+    raise Exception('Major index out of range')
+  minor_index = zero_based_pair_number % g.get_length_minor_colors()
+  if minor_index >= g.get_length_minor_colors():
+    raise Exception('Minor index out of range')
+  return g.get_major_colors_index(major_index), g.get_minor_colors_index(minor_index)
+
+def get_pair_number_from_color(major_color, minor_color):
+  try:
+    major_index = g.get_major_colors_value(major_color)
+  except ValueError:
+    raise Exception('Major index out of range')
+  try:
+    minor_index = g.get_minor_colors_value(minor_color)
+  except ValueError:
+    raise Exception('Minor index out of range')
+  return major_index * g.get_length_minor_colors() + minor_index + 1
