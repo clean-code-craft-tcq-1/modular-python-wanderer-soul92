f2926fc2449f6d4ad716af3004df19673dc5de65
Vivek Tak <57911834+wanderer-soul92@users.noreply.github.com>
2021-02-24 10:51:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 import test
 import color_codes as g
 
-User_manual= g.get_user_manual()
-
+# User_manual= g.get_user_manual()
 
 if __name__ == '__main__':
   test.test_number_to_pair(4, 'White', 'Brown')
@@ -12,5 +11,5 @@ if __name__ == '__main__':
   test.test_pair_to_number('Violet', 'Slate', 25)
   test.test_pair_to_number('Red', 'Orange', 7)
   test.test_user_manual_length(25)
-  print(User_manual)
+  g.print_user_manual()
   print('Done :)')
